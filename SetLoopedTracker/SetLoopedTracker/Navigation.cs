55cdd821abfbf7cbbeec75945e64c24c67a9e12a
JCSpen <103535034+JCSpen@users.noreply.github.com>
2022-10-15 22:56:39 +0100

    Add files via upload

---
+++ SetLoopedTracker/SetLoopedTracker/Navigation.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+    public class Navigation
+    {
+        public int xCord = 0;
+        public int yCord = 0;
+        bool pointExists = false;
+        public Dictionary<string,int> navData = new Dictionary<string,int>();
+        public void Up()
+        {
+            yCord++;
+            alterNavData();
+        }
+
+        public void Down()
+        {
+            yCord--;
+            alterNavData();
+        }
+
+        public void Left()
+        {
+            xCord--;
+            alterNavData();
+        }
+
+        public void Right()
+        {
+            xCord++;
+            alterNavData();
+        }
+
+        public void alterNavData()
+        {
+            pointExists = existingCoordinate();
+            if (pointExists)
+            {
+                navData["(" + xCord + "," + yCord + ")"]++;
+            }
+            else
+                navData.Add("(" + xCord + "," + yCord + ")", 0);
+        }
+
+        bool existingCoordinate()
+        {
+          if(navData.ContainsKey("(" + xCord + "," + yCord + ")"))
+            {
+                return true;
+            }
+            return false;
+
+        }
+        public void sortNavData()
+    {
+        var sortedDict = from entry in navData orderby entry.Value descending select entry;
+        foreach (var value in sortedDict)
+        {
+            Console.WriteLine(value);
+            break;
+        }
+    }
+    }
+
---
+++ SetLoopedTracker/SetLoopedTracker/Program.cs
@@ -0,0 +1,22 @@
+﻿Random rng = new Random();
+Navigation direction = new Navigation();
+for(int i = 0; i<1000000; i++)
+{
+    int randomNum = rng.Next(0, 4);
+    switch(randomNum)
+    {
+        case 0:
+            direction.Up();
+            break;
+        case 1:
+            direction.Down();
+            break;
+        case 2:
+            direction.Left();
+            break;
+        case 3:
+            direction.Right();
+            break;
+    }
+}
+direction.sortNavData();
